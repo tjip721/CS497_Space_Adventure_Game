fde601ad52247f256c7891d4ad22de81fc3fb5b9
KorynLA <leslieak@oregonstate.edu>
2019-07-17 12:49:38 -0700

    Add data development files
    Function outlines

---
+++ spaceAdventure/data.h
@@ -0,0 +1,19 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <string>
+#include <fstream>
+
+
+#define file_holder[500];
+#define save_data="save_log.txt"
+
+//open description file to save to an object variable
+std:string get_file_data(std::string text){}
+
+//save log file conditions
+void save_log(){}
+void open_log(){}
+void remove_log(){}
+
+#endif // DATA_H
