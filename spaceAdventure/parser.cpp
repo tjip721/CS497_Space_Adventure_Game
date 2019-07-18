3f3e4d97d3db96d75bfcfae2f51e1ae87678f62a
jasullivan5 <31702825+jasullivan5@users.noreply.github.com>
2019-07-17 22:35:35 -0700

    Add files via upload

--- spaceAdventure/parser.cpp
+++ spaceAdventure/parser.cpp
@@ -1,38 +1,145 @@
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 #include <sstream>
 #include "Parser.hpp"
 
-Parser::Parser(std::string i)
-{
-    input = i;
-    Parser::inputToLowerCase();
-    Parser::tokenizeInput();
+Parser::Parser() {
+	currentTokenIndex = 0;
 }
 
-void Parser::inputToLowerCase()
-{
+int Parser::loadFiles(std::string verbFileList, std::string nounFileList) {
+	int result;
+	result = loadFileList(verbFileList, verbLists);
+	if (result == EXIT_FAILURE) {
+		return result;
+	}
+	result = loadFileList(nounFileList, nounLists);
+	if (result == EXIT_FAILURE) {
+		return result;
+	}
+}
+
+int Parser::loadFileList(std::string fileListName, std::vector< std::vector<std::string> > &wordLists) {
+    std::string fileName;
+	int result;
+    int listCount = 0;
+	std::ifstream file(fileListName.c_str());
+	if (!file) {
+		std::cerr << "Failed to open file: " << fileListName;
+		return EXIT_FAILURE;
+	}
+	while (getline(file, fileName)) {
+		std::vector<std::string> newList;
+		wordLists.push_back(newList);
+		result = loadFile(fileName, wordLists[listCount]);
+		if (result == EXIT_FAILURE) {
+			return result;
+		}
+		listCount++;
+	}
+	file.close();
+	return EXIT_SUCCESS;
+}
+
+int Parser::loadFile(std::string fileName, std::vector<std::string> &wordList) {
+    std::string word;
+	std::ifstream file(fileName.c_str());
+	if (!file) {
+		std::cerr << "Failed to open file: " << fileName;
+		return EXIT_FAILURE;
+	}
+    while (getline(file, word)) {
+        wordList.push_back(word);
+    }
+    file.close();
+	return EXIT_SUCCESS;
+}
+
+void Parser::processInput(std::string i) {
+	// Reset members
+	inputTokens.clear();
+	verb.clear();
+	nouns.clear();
+	currentTokenIndex = 0;
+
+	// Process new input
+	input = i;
+    inputToLowerCase();
+    tokenizeInput();
+	matchVerb();
+	matchNouns();
+	duplicateNounCheck();
+}
+
+void Parser::inputToLowerCase() {
     std::transform(input.begin(), input.end(), input.begin(), ::tolower);
 }
 
-void Parser::tokenizeInput()
-{
-    std::stringstream inputSS(input);
+void Parser::tokenizeInput() {
+    std::stringstream inputStringStream(input);
     std::string temp;
-    while(std::getline(inputSS, temp, ' '))
-    {
-        tokens.push_back(temp);
+    while (std::getline(inputStringStream, temp, ' ')) {
+        inputTokens.push_back(temp);
     }
 }
 
-void Parser::printTokens()
-{
-    for(int i = 0; i < tokens.size(); i++)
-    {
-        std::cout << tokens[i] << std::endl;
+void Parser::matchVerb() {
+    for (int i = 0; i < inputTokens.size(); i++) {
+        currentTokenIndex++;
+        for (std::vector<std::string> list : verbLists) {
+            if (searchList(inputTokens[i], list)) {
+                verb = list[0];
+                return;
+            }
+        }
     }
 }
 
-void Parser::printString(){
+void Parser::matchNouns() {
+    if (currentTokenIndex < inputTokens.size()) {
+        for (int i = currentTokenIndex; i < inputTokens.size(); i++) {
+            for (std::vector<std::string> list : nounLists) {
+                if (searchList(inputTokens[i], list)) {
+                    nouns.push_back(list[0]);
+                    if (nouns.size() == 2) {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
+
+bool Parser::searchList(std::string token, std::vector<std::string> &list) {
+    int index = lower_bound(std::next(list.begin(), 1), list.end(), token) - list.begin();
+    if (index < list.size() && list[index] == token) {
+        return true;
+    }
+    return false;
+}
+
+void Parser::duplicateNounCheck() {
+	if (nouns.size() > 1 && nouns[0] == nouns[1]) {
+		nouns.pop_back();
+	}
+}
+
+std::string Parser::getVerb() {
+    return verb;
+}
+
+const std::vector<std::string>& Parser::getNouns() const {
+    return nouns;
+}
+
+/************ TEST FUNCTIONS ***************/
+void Parser::printInput() {
     std::cout << input << std::endl;
+}
+
+void Parser::printTokens() {
+    for (std::string word : inputTokens) {
+        std::cout << word << std::endl;
+    }
 }
---
+++ spaceAdventure/parser.hpp
@@ -0,0 +1,42 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+#include <string>
+#include <vector>
+
+class Parser
+{
+    private:
+        // Private Member Variables
+        std::vector< std::vector<std::string> > verbLists;
+        std::vector< std::vector<std::string> > nounLists;
+        std::string input;
+        std::vector<std::string> inputTokens;
+        int currentTokenIndex;
+        std::string verb;
+        std::vector<std::string> nouns;
+
+        // Private Member Functions
+        int loadFileList(std::string fileListName, std::vector< std::vector<std::string> > &wordLists);
+        int loadFile(std::string fileName, std::vector<std::string> &wordList);
+        void inputToLowerCase();
+        void tokenizeInput();
+        void matchVerb();
+        void matchNouns();
+        bool searchList(std::string token, std::vector<std::string> &list);
+		void duplicateNounCheck();
+
+    public:
+        //Public Member Functions
+        Parser();
+		int loadFiles(std::string verbFileList, std::string nounFileList);
+        void processInput(std::string i);
+        std::string getVerb();
+        const std::vector<std::string>& getNouns() const;
+
+        // Test Functions
+        void printInput();
+        void printTokens();
+};
+
+#endif // PARSER_H
