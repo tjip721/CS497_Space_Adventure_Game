6cbc55048d6933c13bf076d818fb83762ae9b5d1
KorynLA <leslieak@oregonstate.edu>
2019-07-21 17:57:45 -0700

    Adding save game

--- spaceAdventure/data.cpp
+++ spaceAdventure/data.cpp
@@ -9,6 +9,29 @@ std::string get_file_data(std::string fileName){
 	r_file.close();
 	return file_read;
 }
-void save_log(){}
+void saveGame(Player::Player player1, std::vector<Area*> planets) {
+   std::fstream savegame;
+   savegame.open("saveLog.txt", std::fstream::out | std::fstream::trunc);
+   savegame << player1.getLife() << " " << player1.getGas() << " " << player1.getLocation()->getName() << " ";
+
+   //add the player inventory
+   std::vector<Item*> playerItems = player1.getPlayerInventory();
+   for(int i=0; i < playerItems.size(); i++) {
+       savegame << playerItems[i]->getName() << " ";
+   }
+   savegame << endl;
+
+   //Add area inventories & firstEntry to text file
+   std::cout << planets.size();
+   for (int j=0; j < planets.size(); j++) {
+      savegame << planets[j]->getAreaEntry() << " ";
+      std::vector<Item*> planetItems= planets[j]->getItems();
+      for (int p=0; p < planetItems.size(); p++) {
+         savegame << planetItems[p]->getName();
+      }
+      savegame << endl;
+   }
+   savegame.close();
+}
 void open_log(){}
 void remove_log(){}
